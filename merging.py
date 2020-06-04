da6ed288c14c36e46a95b19879c96cea61994c74
Daniel Yang <danielyang2002@gmail.com>
2020-06-04 16:29:30 -0500

    Finished up merging stuff

--- merging.py
+++ merging.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 try:
   import pandas as pd
@@ -13,21 +14,34 @@ import sys
 from os import listdir
 from os.path import isfile, join
 
-onlyfiles = [f for f in listdir(r'D:/Downloads/testing/') if isfile(join(r'D:/Downloads/testing/', f))]
+
+username = os.getlogin()
+#folder=sys.argv[1]
+folder = r"D:\Downloads\testing"
+onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))] 
+
+
+
 try:
-  with open(r"C:\Users\Daniel_Yang\Desktop\Test.csv") as f:
-    full_data = pd.read_csv(filepath_or_buffer=f)
+  full_data = pd.read_csv(filepath_or_buffer=f'C:\\Users\\{username}\\Desktop\\MatchData.csv')
+  dir = join(folder, "complete")
+  if not os.path.exists(dir):
+    os.mkdir(dir)
   for i in onlyfiles:
-    with open(join(r'D:/Downloads/testing/', i)) as f:
+    with open(join(folder, i)) as f:
       data = json.load(f)
       data['Match Number']=(f.name[f.name.index('Match ')+6])
       data['Team Number']=(f.name[f.name.index('_ ')+2:f.name.index('.json')])
-  
       full_data = full_data.append(data,ignore_index=True)
-
+    shutil.move(join(folder, i),join(dir,i))
+    
+  
 except FileNotFoundError:
+  dir = join(folder, "complete")
+  if not os.path.exists(dir):
+    os.mkdir(dir)
   for i in onlyfiles:
-    with open(join(r'D:/Downloads/testing/', i)) as f:
+    with open(join(folder, i)) as f:
       data = json.load(f)
       data['Match Number']=(f.name[f.name.index('Match ')+6])
       data['Team Number']=(f.name[f.name.index('_ ')+2:f.name.index('.json')])
@@ -35,7 +49,8 @@ except FileNotFoundError:
         full_data = full_data.append(data,ignore_index=True)
       except:
         full_data = pd.DataFrame(data,index=[0])
+    shutil.move(join(folder, i),join(dir,i))
 
-full_data.to_csv(r"C:\Users\Daniel_Yang\Desktop\MatchData.csv", index=False)
+full_data.to_csv(f'C:\\Users\\{username}\\Desktop\\MatchData.csv', index=False)
 
 
