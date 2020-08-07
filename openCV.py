f27fb621e99c751501f5b876e3ce0a5797197e60
DanielYanger <danielyang2002@gmail.com>
2020-08-06 22:40:09 -0500

    Testing with OpenCV for face

--- openCV.py
+++ openCV.py
@@ -5,7 +5,6 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
-    ret, img = cap.read()
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -18,20 +17,24 @@ while(True):
     # M = cv2.getRotationMatrix2D((cols/2,rows/2),-90,1)
     # dst = cv2.warpAffine(img,M,(cols,rows))
 
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+    #eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    ret,img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(img, 1.1, 4)
     for (x,y,w,h) in faces:
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        # roi_gray = gray[y:y+h, x:x+w]
+        # roi_color = img[y:y+h, x:x+w]
+        #eyes = eye_cascade.detectMultiScale(roi_gray)
+        #for (ex,ey,ew,eh) in eyes:
+         #   cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     # Display the resulting frame
     cv2.imshow('frame',img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
         break
 
 # When everything done, release the capture
