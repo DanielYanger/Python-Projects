0d6cd76b8c10b063625e5ad80b5aaa6fc8ef7cd0
DanielYanger <danielyang2002@gmail.com>
2020-08-12 22:08:23 -0500

    pc

--- moreCV2.py
+++ moreCV2.py
@@ -1,37 +1,44 @@
 import cv2
+from matplotlib import pyplot as plt
 import numpy as np
 
-def nothing(x):
-    pass
+cv2 = cv2.cv2
 
-# Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
-cv2.namedWindow('image')
+cap = cv2.VideoCapture(0)
+while True:
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    kernel = np.ones((35,35),np.uint8)
+    kernel2 = np.ones((5,5),np.uint8)
 
-# create trackbars for color change
-cv2.createTrackbar('R','image',0,255,nothing)
-cv2.createTrackbar('G','image',0,255,nothing)
-cv2.createTrackbar('B','image',0,255,nothing)
+    lower_yellow = np.array([20,80,80])
+    upper_yellow = np.array([60,255,255])
 
-# create switch for ON/OFF functionality
-switch = '0 : OFF \n1 : ON'
-cv2.createTrackbar(switch, 'image',0,1,nothing)
+    mask = cv2.inRange(hsv,lower_yellow,upper_yellow)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    
+    opening = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel2)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    cool = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel2)
 
-while(1):
-    cv2.imshow('image',img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
+    norm_img = cv2.cvtColor(closing,cv2.COLOR_HSV2BGR)
+    closing_gray = cv2.cvtColor(norm_img,cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(closing_gray,100,200)
+    
+    
+    edge_img = cv2.imshow("Edges",edges)
+    #cv2.imshow("Image", norm_img)
+    cv2.imshow("original",frame)
+    #cv2.imshow("gray",gray)
+    #cv2.imshow("hsv",hsv)
+    #cv2.imshow("mask",mask)
+    #cv2.imshow("res",res)
+    cv2.imshow("Final",closing)
+    cv2.imshow("Final_gray",closing_gray)
+    #cv2.imshow("cool",cool)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    # get current positions of four trackbars
-    r = cv2.getTrackbarPos('R','image')
-    g = cv2.getTrackbarPos('G','image')
-    b = cv2.getTrackbarPos('B','image')
-    s = cv2.getTrackbarPos(switch,'image')
-
-    if s == 0:
-        img[:] = 0
-    else:
-        img[:] = [b,g,r]
-
+cap.release()
 cv2.destroyAllWindows()
